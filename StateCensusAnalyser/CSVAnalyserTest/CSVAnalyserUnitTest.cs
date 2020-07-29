9d0401d2577588cf08a13623bfa8128ecc12f46c
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-29 21:50:55 +0530

    [Vaidehi]ADD:testing for incorrect path

--- StateCensusAnalyser/CSVAnalyserTest/CSVAnalyserUnitTest.cs
+++ StateCensusAnalyser/CSVAnalyserTest/CSVAnalyserUnitTest.cs
@@ -13,5 +13,22 @@ namespace CSVAnalyserTest
             int records = StateCensusAnalyser.StateCensusAnalyser.GetStateCensusRecords(filePath);
             Assert.AreEqual(CSVRecords, records);
         }
+
+        [Test]
+        public void GivenCSVFilePath_WhenIncorrect_ShouldThrowCustomException()
+        {
+            try
+            {
+                string filePath = @"E:\CodinClub\Census_Analyser_Problem\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.csv";
+                int CSVRecords = CSVStateCensusAnalyser.GetRecords(filePath);
+                int records = StateCensusAnalyser.StateCensusAnalyser.GetStateCensusRecords(filePath);
+                Assert.AreEqual(CSVRecords, records);
+            }
+            catch (StateCensusAnalyserException e)
+            {
+                //Assert.AreEqual(StateCensusAnalyserException.ExceptionType.INVALID_PATH, e.type);
+                Assert.AreEqual(e.Message,"Entered wrong path");
+            }
+        }
     }
 }
