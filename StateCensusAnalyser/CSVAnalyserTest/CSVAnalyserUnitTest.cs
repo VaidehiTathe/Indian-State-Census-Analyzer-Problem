b68178bf56151b708efbd6b7eb115f8e9db6de4e
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-30 23:05:25 +0530

    [Vaidehi]ADD:test cases for state code file

--- StateCensusAnalyser/CSVAnalyserTest/CSVAnalyserUnitTest.cs
+++ StateCensusAnalyser/CSVAnalyserTest/CSVAnalyserUnitTest.cs
@@ -8,27 +8,27 @@ namespace CSVAnalyserTest
         [Test]
         public void GivenCSVFile_WhenNumberOfRecordsMatches_ShoulReturnTrue()
         {
-            string filePath = @"E:\CodinClub\Assignments_Fellowship\Census_Analyser_Problem\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.csv";
+            string filePath = @"E:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCensusData.csv";
             int CSVRecords = CSVStateCensusAnalyser.GetRecords(filePath);
             int records = StateCensusAnalyser.StateCensusAnalyser.GetStateCensusRecords(filePath);
             Assert.AreEqual(CSVRecords, records);
         }
-
+       
         [Test]
         public void GivenCSVFilePath_WhenIncorrect_ShouldThrowCustomException()
         {
             try
             {
-                string filePath = @"E:\CodinClub\Census_Analyser_Problem\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.csv";
+                string filePath = @"C:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCensusData.csv";
                 int CSVRecords = CSVStateCensusAnalyser.GetRecords(filePath);
                 int records = StateCensusAnalyser.StateCensusAnalyser.GetStateCensusRecords(filePath);
                 Assert.AreEqual(CSVRecords, records);
             }
-            catch (StateCensusAnalyserException e)
+            catch(StateCensusAnalyserException e)
             {
-                //Assert.AreEqual(StateCensusAnalyserException.ExceptionType.INVALID_PATH, e.type);
-                Assert.AreEqual(e.Message, "Entered wrong path");
+                Assert.AreEqual(StateCensusAnalyserException.ExceptionType.INVALID_PATH, e.type);
             }
+            
         }
 
         [Test]
@@ -36,7 +36,7 @@ namespace CSVAnalyserTest
         {
             try
             {
-                string filePath = @"E:\CodinClub\Census_Analyser_Problem\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.txt";
+                string filePath = @"E:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCensusData.csv";
                 int CSVRecords = CSVStateCensusAnalyser.GetRecords(filePath);
                 int records = StateCensusAnalyser.StateCensusAnalyser.GetStateCensusRecords(filePath);
                 Assert.AreEqual(CSVRecords, records);
@@ -44,8 +44,91 @@ namespace CSVAnalyserTest
             catch (StateCensusAnalyserException e)
             {
 
-                Assert.AreEqual("Extension of the file is incorrect", e.Message);
+                Assert.AreEqual(StateCensusAnalyserException.ExceptionType.INVALID_EXTENSION, e.type);
             }
+        }
+
+
+        [Test]
+        public void GivenCSVFile_WhenIncorrectDelimiter_ShouldThrowCustomException()
+        {
+
+            string filePath = @"E:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCensusData.csv";
+            var exception = Assert.Throws<StateCensusAnalyserException>(() => CSVStateCensusAnalyser.checkDelimiters(filePath));//, alternateFilePath));
+            Assert.AreEqual(StateCensusAnalyserException.ExceptionType.DELIMITER_INCORRECT, exception.type);
+        }
+
+        [Test]
+        public void GivenCSVFile_WhenHeadersDoNotMatch_ShouldThrowCustomException()
+        {
+
+            string filePath = @"E:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCensusData.csv";
+            var exception = Assert.Throws<StateCensusAnalyserException>(() => CSVStateCensusAnalyser.HeaderException(filePath));
+            Assert.AreEqual(StateCensusAnalyserException.ExceptionType.HEADERS_MISSMATCH, exception.type);
+
+        }
+
+        [Test]
+        public void GivenCSVStateCodeFile_WhenNumberOfRecordsMatches_ShoulReturnTrue()
+        {
+            string filePath = @"E:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCode.csv";
+            int CSVRecords = CSVStates.GetRecords(filePath);
+            int records = StateCensusAnalyser.StateCodeAnalyser.GetStateCodeRecords(filePath);
+            Assert.AreEqual(CSVRecords, records);
+        }
+
+        [Test]
+        public void GivenCSVStateCodeFilePath_WhenIncorrect_ShouldThrowCustomException()
+        {
+            try
+            {
+                string filePath = @"C:\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCode.csv";
+                int CSVRecords = CSVStates.GetRecords(filePath);
+                int records = StateCensusAnalyser.StateCodeAnalyser.GetStateCodeRecords(filePath);
+                Assert.AreEqual(CSVRecords, records);
+            }
+            catch (StateCensusAnalyserException e)
+            {
+                Assert.AreEqual(StateCensusAnalyserException.ExceptionType.INVALID_PATH, e.type);
+            }
+
+        }
+
+        [Test]
+        public void GivenCSVStateCodeFileName_WhenIncorrectTypeOfFile_ShouldThrowCustomException()
+        {
+            try
+            {
+                string filePath = @"E:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCode.txt";
+                int CSVRecords = CSVStates.GetRecords(filePath);
+                int records = StateCensusAnalyser.StateCodeAnalyser.GetStateCodeRecords(filePath);
+                Assert.AreEqual(CSVRecords, records);
+            }
+            catch (StateCensusAnalyserException e)
+            {
+
+                Assert.AreEqual(StateCensusAnalyserException.ExceptionType.INVALID_EXTENSION, e.type);
+            }
+        }
+
+
+        [Test]
+        public void GivenCSVStateCodeFile_WhenIncorrectDelimiter_ShouldThrowCustomException()
+        {
+
+            string filePath = @"E:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCode.csv";
+            var exception = Assert.Throws<StateCensusAnalyserException>(() => CSVStates.checkDelimiters(filePath));//, alternateFilePath));
+            Assert.AreEqual(StateCensusAnalyserException.ExceptionType.DELIMITER_INCORRECT, exception.type);
+        }
+
+        [Test]
+        public void GivenCSVSTateCodeFile_WhenHeadersDoNotMatch_ShouldThrowCustomException()
+        {
+
+            string filePath = @"E:\CodinClub\Assignments_Fellowship\Indian-State-Census-Analyzer-Problem\StateCensusAnalyser\StateCensusAnalyser\IndiaStateCensusData.csv";
+            var exception = Assert.Throws<StateCensusAnalyserException>(() => CSVStates.HeaderException(filePath));
+            Assert.AreEqual(StateCensusAnalyserException.ExceptionType.HEADERS_MISSMATCH, exception.type);
+
         }
     }
 }
